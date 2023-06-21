e40f125ca4cf8c671b698a11dcebbbc63a8399e4
daydy225 <dabelydadi@gmail.com>
2023-06-21 19:43:01 +0000

    IP address tracker implement Map using react-leaflet and leaflet API update

--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -1,17 +1,51 @@
-import React from 'react'
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { LatLngExpression, } from 'leaflet';
+import React, { useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents, Tooltip,  } from 'react-leaflet';
+
+
+const LocationMarker = () => {
+  const [position, setPosition] = useState<LatLngExpression>([0, 0])
+  const map = useMapEvents({
+    click() {
+      map.locate()
+    },
+    locationfound(location) {
+      // setPosition(e.latlng)
+      setPosition(location.latlng)
+      console.log('location', location)
+      // map.flyTo(location.latlng, map.getZoom())
+    },
+  })
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+      <Tooltip>Tooltip for Marker</Tooltip>
+    </Marker>
+  )
+}
 
 
 
 export const Map:React.FC = () => {
-  
+   
+  const position:LatLngExpression = [51.505, -0.09]
+
+ 
+
   return (
      <MapContainer
-      center={[51.505, -0.09]} // Set the initial map center coordinates
+      center={position} // Set the initial map center coordinates
       zoom={13} // Set the initial zoom level
+      scrollWheelZoom={false} //disable scroll zooming on the map
       style={{ height: '100vh', width: '100%', }} // Set the map container size
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {/* <Marker position={position}>
+      <Tooltip>Tooltip for Marker</Tooltip>
+    </Marker> */}
+    <LocationMarker />
     </MapContainer>
   )
 }
+
--- src/styles/MapContainer.styled.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const StyledMapContainer = styled.div`
-    width: 100%;
-    height: 100vh;
-    position: relative;
-`
